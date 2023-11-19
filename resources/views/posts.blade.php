9bcec5cb079d9e3b29cd1189f17471b93faea9ca
riyadhmobin <riyadhmobin@gmail.com>
2023-11-19 18:19:12 +0600

    first

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>Posts</title>
+</head>
+<body>
+    <article>
+        <h2><a href="/post">My First Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
+
+    <article>
+        <h2><a href="/post">My second Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
+
+    <article>
+        <h2><a href="/post">My Third Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatem omnis a distinctio porro, eum, dignissimos nam adipisci, dolore deserunt fuga? Ratione nisi, laudantium natus maiores voluptatibus doloremque dolore dolorum?Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </article>
+</body>
+</html>
